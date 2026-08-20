879cc640f52dad740f7a3165e1c0e759477d2113
arthurnc1004 <arthurnunesc1004@gmail.com>
2026-08-20 18:01:51 -0300

    rota POST /carteiras e fetch do formulário

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
 import sqlite3
-from flask import Flask
+import os
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__,
+            template_folder=os.path.join(basedir, '../frontend/templates'),
+            static_folder=os.path.join(basedir, '../frontend/static'))
+
 
 def criartabelas():
     conexao = sqlite3.connect('tabelas.db')
     cursor = conexao.cursor()
-
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS carteiras (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,12 +27,53 @@ def criartabelas():
         FOREIGN KEY (carteira_id) REFERENCES carteiras(id)
     )
     """)
-
     conexao.commit()
     conexao.close()
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/carteira')
+def carteira():
+    return render_template('carteira.html')
+
+
+@app.route('/carteiras', methods=['POST'])
+def criar_carteira():
+    dados = request.get_json()
+    print(dados)  # debug
+
+    nome_carteira = dados['nome']
+    lista_criptomoedas = dados['criptomoedas']
+
+    conexao = sqlite3.connect('tabelas.db')
+    cursor = conexao.cursor()
+    cursor.execute("""
+    INSERT INTO carteiras (nome) VALUES (?) 
+    """, (nome_carteira,))
+
+    carteira_id = cursor.lastrowid
+    for criptomoeda in lista_criptomoedas:
+        simbolo = criptomoeda['simbolo']
+        quantidade = criptomoeda['quantidade']
+
+
+        cursor.execute("""
+        INSERT INTO moedascarteira (carteira_id, simbolo, quantidade) VALUES (?, ?, ?)
+        """, (carteira_id, simbolo, quantidade))
+
+    conexao.commit()
+    conexao.close()
+    return {'mensagem': 'Carteira criada'}, 201
+
+
 if __name__ == '__main__':
     criartabelas()
-
-
+    app.run(debug=True)
 
--- frontend/static/js/carteira.js
+++ frontend/static/js/carteira.js
@@ -1,4 +1,7 @@
-const overlaycarteira = document.getElementById('overlay-modal')
+let moedasCarteira = [];
+const divMoedasAdd = document.getElementById('div-moedas-adicionadas');
+
+const overlaycarteira = document.getElementById('overlay-modal');
 
 document.getElementById('btn-adicionar').addEventListener('click', () => {
     overlaycarteira.style.display = 'flex';
@@ -9,8 +12,6 @@ document.querySelectorAll('.btn-fechar-modal').forEach(button => {
         overlaycarteira.style.display = 'none';
     });
 });
-
-
 
 const overlaydetalhes = document.getElementById('overlay-modal-detalhes')
 
@@ -25,3 +26,83 @@ document.querySelectorAll('.btn-fechar-modal').forEach(button => {
         overlaydetalhes.style.display = 'none';
     });
 });
+
+
+
+function atualizarListaMoedas() {
+    divMoedasAdd.innerHTML = '';
+
+    moedasCarteira.forEach((moeda, index) => {
+        const nomeCapitalizado = moeda.simbolo.charAt(0).toUpperCase() + moeda.simbolo.slice(1);
+        divMoedasAdd.innerHTML += `
+            <p class="moeda-adicionada" data-index="${index}">
+                ${moeda.quantidade} - ${nomeCapitalizado}
+                <img class="img-lixeira" src="/static/images/lixeira.png" alt="Remover" data-index="${index}">
+            </p>
+        `;
+    });
+
+    document.querySelectorAll('.img-lixeira').forEach((lixeira) => {
+        lixeira.addEventListener('click', () => {
+            const posicao = parseInt(lixeira.dataset.index);
+            moedasCarteira.splice(posicao, 1);
+            atualizarListaMoedas();
+        });
+    });
+}
+
+
+const addCripto = document.getElementById('btn-adicionar-moeda');
+addCripto.addEventListener('click', () => {
+    const quantidade = document.getElementById('btn-quantidade').value;
+    const criptomoeda = document.getElementById('select-moedas').value;
+
+    if (quantidade) {
+        moedasCarteira.push({simbolo: criptomoeda, quantidade: parseFloat(quantidade)});
+        atualizarListaMoedas();
+        document.getElementById('btn-quantidade').value = '';
+        document.getElementById('select-moedas').value = 'bitcoin';
+    } else {
+        alert('Preencha a quantidade');
+    }
+});
+
+
+document.getElementById('btn-salvar').addEventListener('click', adicionarCarteira);
+
+function adicionarCarteira() {
+
+    const nomeCarteira = document.getElementById('nome-carteira').value;
+
+    if (nomeCarteira && moedasCarteira.length >= 1) {
+
+        const novaCarteira = {
+            nome: nomeCarteira,
+            criptomoedas: moedasCarteira
+        };
+
+        fetch('/carteiras', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(novaCarteira)
+        })
+        .then(resposta => resposta.json())
+        .then(dados => {
+            console.log(dados);
+        })
+        .catch(erro => {
+            console.error('Erro ao salvar carteira:', erro);
+        });
+    
+
+        alert('Carteira adicionada');
+        moedasCarteira = [];
+        atualizarListaMoedas()
+        overlaycarteira.style.display = 'none';
+        document.getElementById('nome-carteira').value = '';
+        document.getElementById('btn-quantidade').value = '';
+        document.getElementById('select-moedas').value = 'bitcoin';
+    } else {
+        alert('Preencha todos os dados');
+    };
+};
