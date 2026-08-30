7a9c6af7e6e1fcdb5b0ed5f502e29e7815675b96
arthurnc1004 <arthurnunesc1004@gmail.com>
2026-08-30 15:22:41 -0300

    feat: calculo do valor de cada moeda no modal de detalhes

--- frontend/static/js/carteira.js
+++ frontend/static/js/carteira.js
@@ -1,10 +1,36 @@
 let moedasCarteira = [];
 let todasCarteiras = [];
+let precosAtuais = {};
+
 const divMoedasAdd = document.getElementById('div-moedas-adicionadas');
 const overlaycarteira = document.getElementById('overlay-modal');
 const overlaydetalhes = document.getElementById('overlay-modal-detalhes')
 const seccarteira = document.getElementById('carteiras')
 
+const ws = new WebSocket('wss://stream.binance.com:9443/stream?streams=btcusdt@ticker/ethusdt@ticker/solusdt@ticker/xrpusdt@ticker/bnbusdt@ticker/usdtbrl@ticker/adausdt@ticker/uniusdt@ticker/dogeusdt@ticker/pepeusdt@ticker/polusdt@ticker/linkusdt@ticker/avaxusdt@ticker/dotusdt@ticker');
+const criptomoedas = {
+    "BTCUSDT": { nome: "Bitcoin", icone: "btc", decimais: 2, moeda: '$'},
+    "ETHUSDT": { nome: "Ethereum", icone: "eth", decimais: 2, moeda: '$'},
+    "SOLUSDT": { nome: "Solana", icone: "sol", decimais: 2, moeda: '$'},
+    "XRPUSDT": { nome: "XRP", icone: "xrp", decimais: 2, moeda: '$'}, 
+    "BNBUSDT": { nome: "BNB", icone: "bnb", decimais: 2, moeda: '$'},
+    "ADAUSDT": { nome: "Cardano", icone: "ada", decimais: 3, moeda: '$'},
+    "USDTBRL": { nome: "USDT", icone: "usdt", decimais: 2, moeda: 'R$'},
+    "UNIUSDT": { nome: "Uniswap", icone: "uni", decimais: 5, moeda: '$'},
+    "DOGEUSDT": { nome: "Dogecoin", icone: "doge", decimais: 5, moeda: '$'},
+    "PEPEUSDT": { nome: "Pepe", icone: "pepe", decimais: 8, moeda: '$'},
+    "POLUSDT": { nome: "Polygon", icone: "pol", decimais: 5, moeda:'$'},
+    "LINKUSDT": { nome: "Chainlink", icone: "link", decimais: 5, moeda: '$'},
+    "AVAXUSDT": { nome: "Avalanche", icone: "avax", decimais: 5, moeda: '$'},
+    "DOTUSDT": { nome: "Polkadot", icone: "dot", decimais: 5, moeda: '$'}
+};
+
+ws.onmessage = (event) => {
+    const resposta = JSON.parse(event.data);
+    const simbolo = resposta.data.s;
+    const preco = parseFloat(resposta.data.c);
+    precosAtuais[simbolo] = preco;
+};
 
 function carregarCarteiras() {
     seccarteira.innerHTML = '';
@@ -27,7 +53,6 @@ function carregarCarteiras() {
             `;
         });
 
-
         const divInformacoes = document.getElementById('informacoes-carteira')
         document.querySelectorAll('.btn-detalhes').forEach(button => {
             button.addEventListener('click', () => {
@@ -42,7 +67,10 @@ function carregarCarteiras() {
                 `
                 const divDetalhesMoedas = document.getElementById('detalhes-moedas');
                 carteiraEncontrada.moedas.forEach((moeda) => {
-                    divDetalhesMoedas.innerHTML += `<p>${moeda.simbolo} - ${moeda.quantidade}</p>`;
+                    const info = criptomoedas[moeda.simbolo];
+                    const precoAtual = precosAtuais[moeda.simbolo] || 0;
+                    const valorTotal = (precoAtual * moeda.quantidade).toFixed(info.decimais);
+                    divDetalhesMoedas.innerHTML += `<p>${moeda.quantidade} - ${info.nome} - ${info.moeda}${valorTotal}</p>`;
                 });
             });
         });
@@ -63,8 +91,9 @@ function carregarCarteiras() {
 }
 carregarCarteiras()
 
-
 document.getElementById('btn-adicionar').addEventListener('click', () => {
+    moedasCarteira = [];
+    atualizarListaMoedas();
     overlaycarteira.style.display = 'flex';
 });
 
@@ -113,7 +142,7 @@ addCripto.addEventListener('click', () => {
         moedasCarteira.push({simbolo: criptomoeda, quantidade: parseFloat(quantidade)});
         atualizarListaMoedas();
         document.getElementById('btn-quantidade').value = '';
-        document.getElementById('select-moedas').value = 'bitcoin';
+        document.getElementById('select-moedas').value = 'BTCUSDT';
     } else {
         alert('Preencha a quantidade');
     }
