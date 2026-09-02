e5a6862dd6f76cc2cb6e5fa8fe85974b990fe14a
arthurnc1004 <arthurnunesc1004@gmail.com>
2026-09-02 13:04:04 -0300

    chore: configura aplicacao para deploy

--- backend/app.py
+++ backend/app.py
@@ -159,6 +159,7 @@ def buscarNoticias():
     
 if __name__ == '__main__':
     criartabelas()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
     
